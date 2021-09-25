fde8c62b6243a35ed423b00328ec80802e791b9e
SSYSS000 <jaessys@protonmail.com>
2021-09-25 22:27:16 +0300

    Fix tests

--- tests/stream.c
+++ tests/stream.c
@@ -26,18 +26,20 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 static int float_rw(void)
 {
-	struct save_stream s = { .stream = tmpfile() };
+	FILE *s = tmpfile();
 	float expected = 50.3f;
 	float got;
 
-	if (!s.stream) {
+	if (!s) {
 		perror("tmpfile");
 		return TEST_FAILURE;
 	}
 
-	sf_put_f32(&s, expected);
-	rewind(s.stream);
-	sf_get_f32(&s, &got);
+	sf_put_f32(s, expected);
+	rewind(s);
+	sf_get_f32(s, &got);
+
+	fclose(s);
 
 	if (expected != got)
 		return TEST_FAILURE;
@@ -48,24 +50,28 @@ REGISTER_TEST(float_rw);
 
 static int vsval_w(void)
 {
-	struct save_stream s = { .stream = tmpfile() };
 	unsigned put_until = VSVAL_MAX + 2;
+	FILE *s = tmpfile();
 	unsigned i;
 	u32 got;
 
-	if (!s.stream) {
+	if (!s) {
 		perror("tmpfile");
 		goto out_fail;
 	}
 
-	for (i = 0u; i <= put_until; ++i)
-		sf_put_vsval(&s, i);
+	for (i = 0u; i <= put_until; ++i) {
+		if (sf_put_vsval(s, i) == EOF) {
+			eprintf("sf_put_vsval(%u) returned EOF\n", i);
+			goto out_fail;
+		}
+	}
 
-	rewind(s.stream);
+	rewind(s);
 
 	for (i = 0u; i <= put_until; ++i) {
-		if (sf_get_vsval(&s, &got) < 0) {
-			eprintf("sf_get_vsval failed.\n");
+		if (sf_get_vsval(s, &got) == EOF) {
+			eprintf("sf_get_vsval() returned EOF\n");
 			goto out_fail;
 		}
 
@@ -76,11 +82,11 @@ static int vsval_w(void)
 		}
 	}
 
-	fclose(s.stream);
+	fclose(s);
 	return TEST_SUCCESS;
 out_fail:
-	if (s.stream)
-		fclose(s.stream);
+	if (s)
+		fclose(s);
 	return TEST_FAILURE;
 }
 REGISTER_TEST(vsval_w);
